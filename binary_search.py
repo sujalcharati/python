d66bc432a0733b79cb7883cb1ed435d2ff9c88a2
sujalcharati <sujalc024@gmail.com>
2024-06-16 10:35:35 +0530

    bin

--- binary_search.py
+++ binary_search.py
@@ -1,2 +1,19 @@
-def binary(arr,l,r,x):
-     
+def binary(arr,left,right,x):
+     while right>=left:
+          mid = left + (right -left)//2
+          if arr[mid] ==x:
+               return mid
+          elif arr[mid] > x:
+               return binary(arr,left,mid-1,x)
+          else:
+               return binary(arr,mid+1,right,x)
+     return -1
+arr = [2,3,4,5,6,7]
+x=5
+ans = binary(arr,0,len(arr)-1,x)
+if ans != -1:
+ print('the element is found at index',ans)
+else:
+     print("the element is not found" )
+
+          
