a1bda3a35d00e09eb034fab1b37bfabfa257afe3
sujalcharati <sujalc024@gmail.com>
2024-06-18 22:11:24 +0530

    edit

--- merge_sort.py
+++ merge_sort.py
@@ -2,7 +2,7 @@ def merge_sort(arr1,arr2):
     i=0
     j=0
     arr3=[]
-    while i< len(arr1) and j < len(arr):
+    while i< len(arr1) and j < len(arr2):
           if arr1[i]< arr2[j]:
              arr3[i]= arr1[i]
              i+=1
