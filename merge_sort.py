c6f8b27f660ccbe744b8bdbc39b5fdadf17989e7
sujalcharati <sujalc024@gmail.com>
2024-06-18 22:08:52 +0530

    edit

--- merge_sort.py
+++ merge_sort.py
@@ -12,6 +12,23 @@ def merge_sort(arr1,arr2):
     while i<len(arr1):
         arr3[i]= arr1[i]
         i+=1
+    while j<len(arr2):
+        arr3[i]= arr2[i]
+        j+=1
+def merge_fxn(arr3):
+    if len(arr3)<2:
+        return arr3
+    mid= len(arr3)//2
+    arr1,arr2 =None,None
+    arr1 = merge_fxn(arr3[:mid])
+    arr2 = merge_fxn(arr3[mid:])
+    return merge_sort(arr1,arr2)
+arr3=[1,2,3,4,5,6,7,8,9,10]
+
+print(merge_fxn(arr3))
+
+
+       
         
 
 
