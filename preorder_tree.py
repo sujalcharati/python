7a19cd9e419bf8249c7428ac76574b364907066d
sujalcharati <sujalc024@gmail.com>
2024-06-23 11:19:51 +0530

    preorder

---
+++ preorder_tree.py
@@ -0,0 +1,21 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+def preorderTraversal(root):
+    result = []
+    if root:
+        result.append(root.val)
+        result.extend(preorderTraversal(root.left))
+        result.extend(preorderTraversal(root.right))
+    return result
+
+root = TreeNode(1)
+root.left = TreeNode(2)
+root.right = TreeNode(3)
+root.left.left = TreeNode(4)
+root.left.right = TreeNode(5)
+
+print(preorderTraversal(root))
