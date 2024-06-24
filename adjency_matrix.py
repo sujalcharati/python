2447525cd751b9cf7d879eb970dcedef7e047ac5
sujalcharati <sujalc024@gmail.com>
2024-06-24 16:58:38 +0530

    matrix

---
+++ adjency_matrix.py
@@ -0,0 +1,15 @@
+def adi_mat(edges):
+    vertices = max(max(u, v) for u, v in edges) + 1
+    adj_matrix = [[0] * vertices for _ in range(vertices)]
+    for u, v in edges:
+        adj_matrix[u][v] = 1
+        adj_matrix[v][u] = 1
+    return adj_matrix
+
+edges = [(0, 1), (0, 2), (1, 3), (1, 4), (2, 5), (2, 6),
+         (3, 7), (4, 8), (5, 9), (6, 7), (7, 8), (8, 9)]
+
+adj_matrix = adi_mat(edges)
+print("Adjacency Matrix:")
+for row in adj_matrix:
+    print(row)
