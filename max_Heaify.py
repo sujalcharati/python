f01df9f0623978e4240e3e09532925d0f9233c4f
sujalcharati <sujalc024@gmail.com>
2024-06-28 18:35:34 +0530

    modify

--- max_Heaify.py
+++ max_Heaify.py
@@ -1,20 +1,19 @@
 arr = [12, 11, 13, 5, 6, 7]
 def maxHeapify(arr, i):
+  largest = i
+  l= 2*i+1 #left array of subtree
+  r= 2*i+2 #right array of subtree
 
-  l= 2*i+1
-  r= (i*2)+2
 
-
-  if l < len(arr) and arr[l] > arr[i]:
+  if l < len(arr) and arr[l] > arr[largest]:
       largest = l
-  else:
-     largest = i
+    
   if r < len(arr) and arr[r] > arr[largest]:
      largest = r
-     if largest != i:
-        arr[largest]= arr[i]
-     val = maxHeapify(arr, largest)
-     print(val)
+  if largest != i:
+        arr[i],arr[largest]= arr[largest],arr[i]
+        maxHeapify(arr, largest)
+print(arr) 
 
 
 
