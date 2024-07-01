aaaea0143a4fa7915702f7309c8918d965673654
sujalcharati <sujalc024@gmail.com>
2024-07-01 10:53:14 +0530

    comment

--- selection_sort.py
+++ selection_sort.py
@@ -1,14 +1,14 @@
- 
+ # here we select an element which is small and we swap it to the left most  index of  array 
 def selection_sort(arr):
-    for i in range(len(arr)):
+    for i in range(len(arr)):# i,j and min here represent index of array
         min = i
         for j in range(min+1,len(arr)):
             if arr[min]> arr[j]:
                min =j
-        arr[i], arr[min]=arr[min], arr[i]
+        arr[i], arr[min]=arr[min], arr[i]# swaping of elements
     return arr 
 array = [5,2,3,4,1]       
-ans = selection_sort(array)
+ans = selection_sort(array)# function call
 print("sorted array:",ans)
 
 
