a63105d73d9a7a5bd5c3a39f747eb67f666cee13
sujalcharati <sujalc024@gmail.com>
2024-07-02 19:08:36 +0530

    comment

--- quick_sort.py
+++ quick_sort.py
@@ -2,7 +2,7 @@ def quick_sort(arr):
     if len(arr) < 2:
         return arr
     mid= len(arr)//2
-    piv = arr[0]
+    piv = arr[0]# it can be any element in the array which is used as reference to  compare
     left=[]
     right=[]
     for i in (arr[1:]):
@@ -10,6 +10,6 @@ def quick_sort(arr):
             left.append(i)
         else:
             right.append(i)
-    return quick_sort(left)+[piv]+quick_sort(right)
+    return quick_sort(left)+[piv]+quick_sort(right)# recursive call
 arr = [8,4,2,3,7,-1,6,78,54,3,2]
 print(quick_sort(arr))
