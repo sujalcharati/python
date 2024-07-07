85e31e935c5946fb89e7289667246b6073547795
sujalcharati <sujalc024@gmail.com>
2024-07-07 11:37:08 +0530

    comment

--- min_jump.py
+++ min_jump.py
@@ -1,7 +1,8 @@
+ # question related to minimum number of jumps we take to reach the final step
 arr = [2,3,1,4,3,1,2]
 def jump(arr):
   n  = len(arr)
-  ans =[float('inf')]*n
+  ans =[float('inf')]*n# infinity
   ans[0]=0
   for i in range(n-1):
     for j in range (1,arr[i]+1):
